02a5109f5ee960db169e81c5ed1da54921bd17dc
Swetha-Nagarajan <swethagrace92@gmail.com>
2023-03-06 06:03:31 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,2 +1,26 @@
 # Create PBP resource naming module
 
+locals {
+  component_order = { for idx, component in var.name_components_order: component => idx }
+}
+
+resource "random_id" "service_name_suffix" {
+  byte_length = 4
+}
+
+locals {
+  service_name_random = length(trim(var.service_name)) > 0 ? "-${var.service_name}" : "-${random_id.service_name_random.hex}"
+}
+
+locals {
+  name_components = {
+    pbp     = "pbp",
+    domain  = var.domain,
+    service_name = local.service_name_random,
+    environment  = var.environment
+  }
+}
+
+locals {
+  name_components_ordered = { for component in var.name_components_order: component => local.name_components[component] if contains(keys(local.name_components), component) }
+}
