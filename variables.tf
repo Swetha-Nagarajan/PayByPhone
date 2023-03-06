d9160819a66f46e52f7fb5f4718a943d70322177
Swetha-Nagarajan <swethagrace92@gmail.com>
2023-03-06 05:13:09 -0800

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,27 +1,30 @@
-variable "domain" {
+# Input variable definitions
+
+
+variable "pbp_domain" {
   type        = string
-  description = "The domain of the AWS resource"
+  description = "The domain of the PBP AWS resource"
   validation {
-    condition     = can(index(["reporting", "data", "platform", "frontend", "backend"], lower(var.domain)))
+    condition     = can(index(["reporting", "data", "platform", "frontend", "backend"], lower(var.pbp_domain)))
     error_message = "Domain must be one of reporting, data, platform, frontend, or backend"
   }
 }
 
-variable "service_name" {
+variable "pbp_service_name" {
   type        = string
-  default     = random_id.service_name_suffix.hex
-  description = "The name of the service for the AWS resource"
+  default     = random_id.pbp_service_name_suffix.hex
+  description = "The name of the service for the PBP AWS resource"
   validation {
-    condition     = can(regex("^[a-zA-Z0-9]*$", var.service_name))
+    condition     = can(regex("^[a-zA-Z0-9]*$", var.pbp_service_name))
     error_message = "Service name must be alphanumeric and contain no spaces"
   }
 }
 
-variable "environment" {
+variable "pbp_environment" {
   type        = string
-  description = "The environment of the AWS resource"
+  description = "The environment of the PBP AWS resource"
   validation {
-    condition     = can(index(["development", "production"], lower(var.environment)))
+    condition     = can(index(["development", "production"], lower(var.pbp_environment)))
     error_message = "Environment must be one of development or production"
   }
 }
