d67c28dca47a4a7301da7f8e1208b8cb723be605
Swetha-Nagarajan <swethagrace92@gmail.com>
2023-03-06 06:30:03 -0800

    Create versions.tf

---
+++ versions.tf
@@ -0,0 +1,17 @@
+#terraform block 
+
+terraform {
+  required_version = ">= 1.0" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = "us-west-2"
+  profile = "default"
+}
