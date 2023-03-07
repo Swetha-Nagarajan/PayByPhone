50d8336b58ac28d730dbf180619a1c395ae43207
Swetha-Nagarajan <swethagrace92@gmail.com>
2023-03-07 10:08:11 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,4 +7,13 @@ provider "aws" {
 
 module "aws_resource_naming" {
   source = "./aws_resource_naming"
+
+  domain      = "data"
+  environment = "production"
+  service_name = "foobar"
+}
+
+  
+resource "aws_s3_bucket" "naming_bucket" {
+bucket = module.aws_resource_naming.name
 }
