067b2d54832d440982245baf5ddef623d96a1c6f
Swetha-Nagarajan <swethagrace92@gmail.com>
2023-03-07 09:58:24 -0800

    Update main.tf

--- aws_resource_naming/main.tf
+++ aws_resource_naming/main.tf
@@ -1,1 +1,28 @@
 
+#missed provider block ( added )
+# Provider Block
+provider "aws" {
+  region  = "us-west-2"
+  profile = "default"
+}
+
+# Define random_id resource
+
+resource "random_id" "service_name_random" {
+  byte_length = 4
+}
+
+
+# Define locals block
+
+locals {
+  reordered_components = sort(var.components_list)
+  name_components = [
+    "pbp",
+    var.domain,
+    var.service_name != random_id.service_name_random.hex ? var.service_name : null,
+    var.environment
+  ]
+  name_components = [for c in name_components : c if c != null]
+  name = join(var.delimiter, name_components)
+}
