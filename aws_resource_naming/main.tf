3b2aea58f82fb9a695c08ff758127d89cfe9d2e0
Swetha-Nagarajan <swethagrace92@gmail.com>
2023-03-07 10:15:59 -0800

    Update main.tf

--- aws_resource_naming/main.tf
+++ aws_resource_naming/main.tf
@@ -1,5 +1,15 @@
+#terraform block 
 
-#missed provider block ( added )
+terraform {
+  required_version = ">= 1.0" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
 # Provider Block
 provider "aws" {
   region  = "us-west-2"
