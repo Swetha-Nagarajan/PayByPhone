f813112af20530921c04b32f9803ffcce6f38a32
Swetha-Nagarajan <swethagrace92@gmail.com>
2023-03-07 09:59:38 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,24 +5,6 @@ provider "aws" {
   profile = "default"
 }
 
-# Define random_id resource
-
-resource "random_id" "service_name_random" {
-  byte_length = 4
+module "aws_resource_naming" {
+  source = "./aws_resource_naming"
 }
-
-
-# Define locals block
-
-locals {
-  reordered_components = sort(var.components_list)
-  name_components = [
-    "pbp",
-    var.domain,
-    var.service_name != random_id.service_name_random.hex ? var.service_name : null,
-    var.environment
-  ]
-  name_components = [for c in name_components : c if c != null]
-  name = join(var.delimiter, name_components)
-}
-   
